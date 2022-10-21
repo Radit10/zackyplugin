743406f5fbf13abed8f97a69a2e3cded77674ebe
Radit10 <nayakaradin@gmail.com>
2022-10-21 08:34:49 +0700

    ZackyMD-Plugin

---
+++ Procfile
@@ -0,0 +1,1 @@
+node . --db "mongodb+srv://RaditTok:nayaka123@cluster0.dytrn2e.mongodb.net/?retryWrites=true&w=majority" --autocleartmp --restrict
---
+++ config.js
@@ -0,0 +1,126 @@
+//Ini untuk setting Bot
+//Untuk gambar background menu ada -
+//Dalam file _menunya.js
+
+//『 Utama 』 //
+global.owner = ['6285236483504','62852364835043']  // isi jika perlu
+global.mods = ['6285236483504','62852364835043']   // isi jika perlu
+global.prems = ['6285236483504','62852364835043']  // isi jika perlu
+
+//Isi dengan nama dan nomor lu//
+global.nameowner = 'Mursid S' // isi nama kalian bebas
+global.numberowner = '62852364835043' // isi nomor kalian dengan awalan seperti di contoh 
+global.dana = '6285236483504' // isi dengan nomor dana yg kamu punya 
+global.pulsa = '6285236483504' // isi dengan nomor pulsa hp kalian
+global.gopay = '6285236483504'
+
+//Name Bot & Grouplink//
+global.namebot = 'мυʀѕι∂ вσт-χмℓ'
+global.gc = 'https://chat.whatsapp.com/FhSBFvhtzfN5hIVe1GXAfj'
+global.web = 'https://instagram.com/mursid.st' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
+
+//Tanpilan wm Bot //
+global.lolkey = 'rey2k22' //biar mudah ngegantinya semisal apikeynya expired:v
+global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
+global.wm = 'мυʀѕι∂ вσт-χмℓ'
+global.watermark = wm
+global.wm2 = 'мυʀѕι∂ вσт-χмℓ'
+global.wm3 = 'мυʀѕι∂ вσт-χмℓ'
+global.wm4 = '2022'
+global.htki = '––––––「' 
+global.htka = '」––––––'
+global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text='
+global.wait = 'тυɴggυ ѕє∂αɴg ∂ι ρʀσѕєѕ...'
+global.eror = 'ѕєʀνєʀ ѕє∂αɴg єʀʀσʀ'
+global.benar = 'вєɴαʀ'
+global.salah = 'ѕαℓαн'
+global.stiker_wait = 'Loading...'
+global.packname = 'sᴛɪᴄᴋᴇʀ ʙʏ'
+global.author = 'мυʀѕι∂ вσт-χмℓ'
+
+//Apikey Setting//
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  amel: 'https://melcanz.com',
+    bx: 'https://bx-hunter.herokuapp.com',
+  dhnjing: 'https://dhnjing.xyz',
+  hardianto: 'https://hardianto-chan.herokuapp.com',
+  jonaz: 'https://jonaz-api-v2.herokuapp.com',
+  neoxr: 'https://neoxr-api.herokuapp.com',
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  nzcha: 'http://nzcha-apii.herokuapp.com',
+  bg: 'http://bochil.ddns.net',
+  fdci: 'https://api.fdci.se',
+  dzx: 'https://api.dhamzxploit.my.id',
+  bsbt: 'https://bsbt-api-rest.herokuapp.com',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.xyz',
+  zekais: 'http://zekais-api.herokuapp.com',
+  hardianto: 'https://hardianto-chan.herokuapp.com',
+  pencarikode: 'https://pencarikode.xyz', 
+  erdwepe: 'https://erdwpe-api.herokuapp.com',
+  lolhuman: 'https://api.lolhuman.xyz',
+  LeysCoder: 'https://leyscoders-api.herokuapp.com',
+  rey: 'https://server-api-rey.herokuapp.com',
+  males: 'https://malesin.xyz'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://melcanz.com': 'elaina',
+  'https://server-api-rey.herokuapp.com': 'apirey',
+  'https://api.xteam.xyz': 'd37372311698ed1d',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://bsbt-api-rest.herokuapp.com': 'benniismael',
+  'https://api.zeks.xyz': 'apivinz',
+  'https://hardianto-chan.herokuapp.com': 'hardianto',
+  'https://pencarikode.xyz': 'pais', 
+  'https://leyscoders-api.herokuapp.com': 'dappakntlll',
+  'https://zekais-api.herokuapp.com': 'apikeymu',
+  'https://api.lolhuman.xyz': 'Deffbotz',
+}
+
+//Setting Rpg//
+global.multiplier = 69 // The higher, The harder levelup
+global.rpg = {
+  emoticon(string) {
+    string = string.toLowerCase()
+    let emot = {
+      exp: '✉️',
+      money: '💵',
+      potion: '🥤',
+      diamond: '💎',
+      common: '📦',
+      uncommon: '🎁',
+      mythic: '🗳️',
+      legendary: '🗃️',
+      pet: '🎁',
+      sampah: '🗑',
+      armor: '🥼',
+      sword: '⚔️',
+      kayu: '🪵',
+      batu: '🪨',
+      string: '🕸️',
+      kuda: '🐎',
+      kucing: '🐈' ,
+      anjing: '🐕',
+      petFood: '🍖',
+      gold: '👑',
+      emerald: '💚'
+    }
+    let results = Object.keys(emot).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
+    if (!results.length) return ''
+    else return emot[results[0][0]]
+  }
+}
+
+//Jangan merubah bagian ini! //
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
